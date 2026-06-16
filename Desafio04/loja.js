76488de363aa65dd42706df86ac1eb409432991c
Murilo-Mlo <murilolupy@gmail.com>
2026-06-15 21:53:40 -0300

    Deasfio04

---
+++ Desafio04/loja.js
@@ -0,0 +1,27 @@
+/*Desafio - 04 - Loja Tech
+
+Uma Loja de informatica possui varios produtos cadastrados.
+
+Crie um array de objetos contendo:
+
+- nome
+- preco
+
+utilize uma estrutura de repetição para monstrar apenas os produtos que custam mais de R$ 100,00.
+
+Exiba o nome dos produtos encontrados.
+*/
+//                  0           1         2        3       4   
+let produto = ["Computador", "Mouse", "Fone", "Teclado", "Monitor"];
+let preco = [100, 25, 50, 75, 100];
+let posicao = 0
+
+function checarpreco() {
+    if (posicao < 5) {
+
+
+
+
+    }
+    posicao = posicao +1
+}
